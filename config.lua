d3681af8a339b026391230c079c8bd12de554265
yukad2 <myuratq@gmail.com>
2024-01-25 00:08:31 +0900

    判定増大

--- config.lua
+++ config.lua
@@ -10,29 +10,29 @@ Config.CoralLocations = {
             Coral = {
                 [1] = {
                     coords = vector3(-2849.25, -377.58, -40.23),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
                     coords = vector3(-2838.43, -363.63, -39.45),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
                     coords = vector3(-2887.04, -394.87, -40.91),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [4] = {
                     coords = vector3(-2808.99, -385.56, -39.32),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
@@ -48,22 +48,22 @@ Config.CoralLocations = {
             Coral = {
                 [1] = {
                     coords = vector3(-3275.03, -38.58, -19.21),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
                     coords = vector3(-3273.73, -76.0, -26.81),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
                     coords = vector3(-3346.53, -50.4, -35.84),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
@@ -79,22 +79,22 @@ Config.CoralLocations = {
             Coral = {
                 [1] = {
                     coords = vector3(-3388.01, 1635.88, -39.41),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
                     coords = vector3(-3354.19, 1549.3, -38.21),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
                     coords = vector3(-3326.04, 1636.43, -40.98),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
@@ -110,22 +110,22 @@ Config.CoralLocations = {
             Coral = {
                 [1] = {
                     coords = vector3(2978.05, -1509.07, -24.96),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
                     coords = vector3(3004.42, -1576.95, -29.36),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
                     coords = vector3(2951.65, -1560.69, -28.36),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
@@ -141,29 +141,29 @@ Config.CoralLocations = {
             Coral = {
                 [1] = {
                     coords = vector3(3421.69, 1976.54, -50.64),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
                     coords = vector3(3424.07, 1957.46, -53.04),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
                     coords = vector3(3434.65, 1993.73, -49.84),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [4] = {
                     coords = vector3(3415.42, 1965.25, -52.04),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
@@ -179,29 +179,29 @@ Config.CoralLocations = {
             Coral = {
                 [1] = {
                     coords = vector3(2724.0, -2134.95, -19.33),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
                     coords = vector3(2710.68, -2156.06, -18.63),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
                     coords = vector3(2702.84, -2139.29, -18.51),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [4] = {
                     coords = vector3(2736.27, -2153.91, -20.88),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
@@ -217,29 +217,29 @@ Config.CoralLocations = {
             Coral = {
                 [1] = {
                     coords = vector3(542.31, 7245.37, -30.01),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
                     coords = vector3(528.21, 7223.26, -29.51),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
                     coords = vector3(510.36, 7254.97, -32.11),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [4] = {
                     coords = vector3(525.37, 7259.12, -30.51),
-                    length = 3,
-                    width = 3,
+                    length = 7,
+                    width = 7,
                     heading = 100.0,
                     PickedUp = false
                 },
